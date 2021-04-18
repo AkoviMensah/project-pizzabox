85a974908fa926ba7f2acab823b222585c3542e7
Akovi Mensah <akovi66@gmail.com>
2021-04-18 00:17:23 -0500

    edited Program.cs

--- PizzaBox.Client/Program.cs
+++ PizzaBox.Client/Program.cs
@@ -1,12 +1,114 @@
 ﻿using System;
+using PizzaBox.Domain.Abstracts;
+using PizzaBox.Domain.Models;
+using PizzaBox.Client.Singletons;
 
 namespace PizzaBox.Client
 {
-    public class Program
+  /// <summary>
+  /// 
+  /// </summary>
+  public class Program
+  {
+    private static readonly PizzaSingleton _pizzaSingleton = PizzaSingleton.Instance;
+    private static readonly StoreSingleton _storeSingleton = StoreSingleton.Instance;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void Main()
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to PizzaBox");
-        }
+      Run();
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void Run()
+    {
+      var order = new Order();
+
+      Console.WriteLine("Welcome to PizzaBox");
+      PrintStoreList();
+
+      order.Customer = new Customer();
+      order.Store = SelectStore();
+      order.Pizza = SelectPizza();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void PrintOrder(APizza pizza)
+    {
+      Console.WriteLine($"Your order is: {pizza}");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void PrintPizzaList()
+    {
+      Console.WriteLine("Here are the available pizzas");
+      var index = 0;
+
+      foreach (var item in _pizzaSingleton.Pizzas)
+      {
+        Console.WriteLine($"{++index} - {item}");
+      }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void PrintStoreList()
+    {
+      Console.WriteLine("Please select a store:");
+      var index = 0;
+
+      foreach (var item in _storeSingleton.Stores)
+      {
+        Console.WriteLine($"{++index} - {item}");
+      }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private static APizza SelectPizza()
+    {
+      Console.WriteLine("Select a pizza:");
+      var valid = int.TryParse(Console.ReadLine(), out int input);
+
+      if (!valid)
+      {
+        return null;
+      }
+
+      var pizza = _pizzaSingleton.Pizzas[input - 1];
+
+      PrintOrder(pizza);
+
+      return pizza;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private static AStore SelectStore()
+    {
+      var valid = int.TryParse(Console.ReadLine(), out int input);
+
+      if (!valid)
+      {
+        return null;
+      }
+
+      PrintPizzaList();
+
+      return _storeSingleton.Stores[input - 1];
+    }
+  }
 }
