a8a97a2f38ef2547bd72c0d16bd28ea48afa7dc0
Akovi Mensah <akovi66@gmail.com>
2021-04-17 23:22:53 -0500

    added Singletons

---
+++ PizzaBox.Client/Singletons/StoreSingleton.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using PizzaBox.Domain.Abstracts;
+using PizzaBox.Storing.Repositories;
+
+namespace PizzaBox.Client.Singletons
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class StoreSingleton
+  {
+    private const string _path = @"data/stores.xml";
+    private readonly FileRepository _fileRepository = new FileRepository();
+    private static StoreSingleton _instance;
+
+    public List<AStore> Stores { get; }
+    public static StoreSingleton Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = new StoreSingleton();
+        }
+
+        return _instance;
+      }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private StoreSingleton()
+    {
+      if (Stores == null)
+      {
+        Stores = _fileRepository.ReadFromFile<List<AStore>>(_path);
+      }
+    }
+  }
+}
